ce60721dd9ff3944d69188d626a0c92b656316f2
PepGames <joshuapepoli@gmail.com>
2023-09-13 21:13:20 -0400

    Updated Masters

--- plugin_sync/__init__.py
+++ plugin_sync/__init__.py
@@ -1,5 +1,4 @@
-from PyQt5.QtGui import QIcon
-
+import re
 import mobase
 
 class GamePluginsRequirement(mobase.IPluginRequirement):
@@ -20,27 +19,30 @@ class PluginSync(mobase.IPluginTool):
     _organizer: mobase.IOrganizer
     _modList: mobase.IModList
     _pluginList: mobase.IPluginList
+    _pluginGame: mobase.IPluginGame
 
     def __init__(self):
         super().__init__()
 
-    def init(self, organizer: mobase.IOrganizer):
+    def init(self, organizer: mobase.IOrganizer, pluginGame: mobase.IPluginGame):
         self._organizer = organizer
+        self._pluginGame = pluginGame
         self._modList = organizer.modList()
         self._pluginList = organizer.pluginList()
+        self._gameName = pluginGame.gameName()
         return True
 
     def name(self):
         return "Sync Plugins (Universal)"
 
     def author(self):
-        return "coldrifting & SuperPep"
+        return "coldrifting, and SuperPep"
 
     def description(self):
         return "Syncs plugin load order with mod order"
 
     def version(self):
-        return mobase.VersionInfo(1, 0, 0, mobase.ReleaseType.FINAL)
+        return mobase.VersionInfo(2, 0, 0, mobase.ReleaseType.FINAL)
 
     def isActive(self):
         return (self._organizer.managedGame().feature(mobase.GamePlugins))
@@ -58,7 +60,33 @@ class PluginSync(mobase.IPluginTool):
             key=lambda
             plugin: self._modList.priority(self._pluginList.origin(plugin))
         )
+        
+        # Split into two lists, master files and regular plugins
+        plugins = []
+        masters = []
+        for plugin in allPlugins:
+            if (self._pluginList.isMaster(plugin)):
+                masters.append(plugin)
+            else:
+                plugins.append(plugin)
+        
+        
+        # Sort DLC correctly (Unmanaged Files)
+        if (self._gameName == "Skyrim"):
+         masters[0] = "Skyrim.esm"
+         masters[1] = "Update.esm"
+         masters[2] = "Dawnguard.esm"
+         masters[3] = "HearthFires.esm"
+         masters[4] = "Dragonborn.esm"
+        elif (self._gameName == "Morrowind"):
+         masters[0] = "Morrowind.esm"
+         masters[1] = "Tribunal.esm"
+         masters[2] = "Bloodmoon.esm"
+        else: []
+         
 
+        # Merge masters into the plugin list at the begining
+        allPlugins = masters + plugins
 
         # Set load order
         self._pluginList.setLoadOrder(allPlugins)
@@ -79,7 +107,7 @@ class PluginSync(mobase.IPluginTool):
         return "Enables all Mods one at a time to match load order"
 
     def icon(self):
-        return QIcon()
+        return []
 
     def requirements(self):
         return [GamePluginsRequirement()]
